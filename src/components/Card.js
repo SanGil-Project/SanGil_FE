da49485194093fa0ed5bf05ff13f4ed608e35957
Kopite93 <1000nulee@naver.com>
2022-05-02 21:42:46 +0900

    220502

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,14 @@ import React from "react";
 import Grid from "../elements/Grid";
 
 const Card = (props) => {
-  const { width, height, margin, border } = props;
+  const { width, height, margin, border, maxWidth } = props;
   return (
     <Grid
       border={border}
       width={width}
       height={height}
       margin={margin}
+      maxWidth={maxWidth}
       radius="10px"
       shadow="0 4px 12px rgba(0, 0, 0, 0.1) "
     >
--- src/elements/Image.js
+++ src/elements/Image.js
@@ -19,13 +19,13 @@ Image.defaultProps = {
 const Img = styled.img`
   ${(props) => (props.width ? `width: ${props.width};` : null)}
   ${(props) => (props.height ? `height: ${props.height};` : null)}
-    ${(props) => (props.margin ? `margin: ${props.margin};` : null)}
-  // object-fit: scale-down;
+    ${(props) =>
+    props.margin ? `margin: ${props.margin};` : null} // object-fit: scale-down;
 `;
 
 const CircleImg = styled.img`
-  ${(props) => (props.height ? `color: ${props.height};` : null)}
-  ${(props) => (props.width ? `color: ${props.width};` : null)}
+  ${(props) => (props.height ? `height: ${props.height};` : null)}
+  ${(props) => (props.width ? `width: ${props.width};` : null)}
   ${(props) => (props.margin ? `margin: ${props.margin};` : null)}
   border-radius: 100%;
   object-fit: cover;
