7f850a74de98d015d05f2ea1649472aa0b8e4aa5
parkksss <yesleee9211@gmail.com>
2022-05-02 23:29:56 +0900

    리스트 마커 띄우기

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -7,6 +7,37 @@ import { actionCreators as pathActions } from "../redux/modules/geolocation";
 const { kakao } = window;
 
 const KakaoMap = (props) => {
+
+  const positions = [
+    {
+        title: '북한산', 
+        latlng: {
+          lat: 37.65928568119137,
+          lng: 126.97733384051244,
+        },
+    },
+    {
+        title: '관악산',  
+        latlng: {
+          lat: 37.44466683008581,
+          lng: 126.96388884210135,
+        }
+    },
+    {
+        title: '지리산',  
+        latlng: {
+          lat: 35.337592276835075,
+          lng: 127.73052130599065,
+        }
+    },
+    {
+        title: '가야산', 
+        latlng: {
+          lat: 35.82281671579307,
+          lng: 128.12301774151817,
+        }
+    }
+];
   const { width, height, margin, maxWidth, level, type } = props;
   const [map, setMap] = useState();
   const [location, setLocation] = useState({
@@ -64,7 +95,15 @@ const KakaoMap = (props) => {
         style={{ width: "100%", height: "100%", borderRadius: "12px" }}
         onCreate={setMap}
       >
-        <ZoomControl position={kakao.maps.ControlPosition.TOPLEFT} /></Map> : 
+        <ZoomControl position={kakao.maps.ControlPosition.TOPLEFT} />
+        {positions.map((p, idx)=>{ 
+          console.log(p);
+          return (
+            <MapMarker key={idx} position={{...p.latlng}}>
+              <div style={{ color: "#000" }}>{p.title}</div>
+            </MapMarker>);
+        })}
+        </Map> : 
         <Map
         center={location}
         level={level}
