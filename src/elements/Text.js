bf49af86df6eae401e837d0b9670f7bec5eed353
Kopite93 <1000nulee@naver.com>
2022-05-02 09:49:15 +0900

    220502

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Text = (props) => {
-  const { children, bold, color, size, margin, align,} = props;
+  const {
+    children,
+    bold,
+    color,
+    size,
+    margin,
+    align,
+    width,
+    height,
+    lineHeight,
+    maxWidth,
+  } = props;
   const styles = {
     bold: bold,
     color: color,
     size: size,
     margin: margin,
     align: align,
+    width,
+    height,
+    lineHeight,
+    maxWidth,
   };
   return (
     <React.Fragment>
       <P {...styles}>{children}</P>
     </React.Fragment>
   );
-}
+};
 
 Text.defaultProps = {
   children: null,
   bold: "400",
-  color: '#222831',
-  size: '16px',
+  color: "#222831",
+  size: "16px",
   margin: false,
-  align: 'center',
+  align: "start",
 };
 
 const P = styled.p`
@@ -31,8 +46,12 @@ const P = styled.p`
   color: ${(props) => props.color};
   font-size: ${(props) => props.size};
   font-weight: ${(props) => props.bold};
-  ${(props) => (props.margin ? `margin: ${props.margin};` : '')}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : null)}
   text-align: ${(props) => props.align};
+  ${(props) => (props.width ? `width: ${props.width};` : null)}
+  ${(props) => (props.height ? `height: ${props.height};` : null)}
+  ${(props) => (props.lineHeight ? `line-height: ${props.lineHeight};` : null)}
+  ${(props) => (props.maxWidth ? `max-width: ${props.maxWidth};` : null)}
 `;
 
 export default Text;
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "../elements/Grid";
 import Icon from "../elements/Icon";
+import Text from "../elements/Text";
 import { HorizontalScroll, Card, Header } from "../components/component";
 import { Desktop } from "../shared/responsive";
 
@@ -13,22 +14,17 @@ const Main = () => {
       <Grid border="1px solid black" width="414px" margin="0 auto">
         <Header />
         <Grid padding="7px" overflowY="scroll" height="844px">
-          <Grid
-            height="453px"
-            margin="0 auto 60px auto"
-          >
-            <p
-              style={{
-                width: "350px",
-                height: "24px",
-                margin: "30px 0 0 7px",
-                fontWeight: "600",
-                fontSize: "2rem",
-                lineHeight: "24px",
-              }}
+          <Grid height="453px" margin="0 auto 60px auto">
+            <Text
+              width="350px"
+              height="24px"
+              margin="30px 0 0 7px"
+              bold="600"
+              size="2rem"
+              lineHeight="24px"
             >
               지금 산길러 들이 오르고있는 산 TOP 5
-            </p>
+            </Text>
             <Card
               border="2px solid #B3B3B3"
               width="386px"
@@ -36,18 +32,16 @@ const Main = () => {
               margin="34px auto 0 auto"
             >
               <Icon width="34px" height="29px" type="rank" />
-              <p
-                style={{
-                  width: "9px",
-                  height: "17px",
-                  fontSize: "1.4rem",
-                  fontWeight: "300",
-                  margin: "-32px 14px",
-                  color: "#fff",
-                }}
+              <Text
+                width="9px"
+                height="17px"
+                size="1.4rem"
+                bold="300"
+                margin="-32px 14px"
+                color="#fff"
               >
                 1
-              </p>
+              </Text>
               <Icon type="like" width="18px" margin="0 0 -113px 355px" />
             </Card>
             <Grid height="30px">
@@ -56,26 +50,22 @@ const Main = () => {
                   display: "flex",
                 }}
               >
-                <p
-                  style={{
-                    maxWidth: "130px",
-                    margin: "10px 8px 0 7px",
-                    fontWeight: "600",
-                    fontSize: "1.4rem",
-                  }}
+                <Text
+                  maxWidth="130px"
+                  margin="10px 8px 0 7px"
+                  bold="600"
+                  size="1.4rem"
                 >
                   충청남도 서산시
-                </p>
-                <p
-                  style={{
-                    margin: "10px 0 0 0",
-                    width: "240px",
-                    fontWeight: "200",
-                    fontSize: "1.4rem",
-                  }}
+                </Text>
+                <Text
+                  margin="10px 0 0 0"
+                  width="240px"
+                  bold="200"
+                  size="1.4rem"
                 >
                   산이름졸라길다아아아아아아앙?
-                </p>
+                </Text>
               </div>
             </Grid>
 
@@ -84,18 +74,16 @@ const Main = () => {
                 <div key={idx}>
                   <Card width="194px" height="120px" margin="34px 7px 8px 7px">
                     <Icon width="34px" height="29px" type="rank" />
-                    <p
-                      style={{
-                        width: "9px",
-                        height: "17px",
-                        fontSize: "1.4rem",
-                        fontWeight: "300",
-                        margin: "-32px 14px",
-                        color: "#fff",
-                      }}
+                    <Text
+                      width="9px"
+                      height="17px"
+                      size="1.4rem"
+                      bold="300"
+                      margin="-32px 14px"
+                      color="#fff"
                     >
                       {cur}
-                    </p>
+                    </Text>
                     <Icon
                       type="like"
                       width="18px"
@@ -103,25 +91,17 @@ const Main = () => {
                       margin="0 0 -117px 163px"
                     />
                   </Card>
-                  <p
-                    style={{
-                      maxWidth: "130px",
-                      margin: "10px 8px 0 7px",
-                      fontWeight: "600",
-                      fontSize: "1.4rem",
-                    }}
+                  <Text
+                    maxWidth="130px"
+                    margin="10px 8px 0 7px"
+                    bold="600"
+                    size="1.4rem"
                   >
                     충청남도 서산시
-                  </p>
-                  <p
-                    style={{
-                      margin: "10px 0 0 7px",
-                      fontWeight: "200",
-                      fontSize: "1.4rem",
-                    }}
-                  >
+                  </Text>
+                  <Text margin="10px 0 0 7px" bold="200" size="1.4rem">
                     산이름졸라길다아아아아아아앙
-                  </p>
+                  </Text>
                 </div>
               ))}
             </HorizontalScroll>
@@ -132,18 +112,16 @@ const Main = () => {
             margin="0 auto 60px auto"
             height="238px"
           >
-            <p
-              style={{
-                width: "350px",
-                height: "24px",
-                margin: "0 7px 24px 7px",
-                fontWeight: "600",
-                fontSize: "2rem",
-                lineHeight: "24px",
-              }}
+            <Text
+              width="350px"
+              height="24px"
+              margin="0 7px 24px 7px"
+              bold="600"
+              size="2rem"
+              lineHeight="24px"
             >
               👀 주변 산길
-            </p>
+            </Text>
             <HorizontalScroll>
               {num.map((cur, idx) => (
                 <div key={idx}>
@@ -155,15 +133,13 @@ const Main = () => {
                       margin="0 0 -103px 163px"
                     />
                   </Card>
-                  <p
-                    style={{
-                      margin: "8px 0 0 7px",
-                      fontWeight: "600",
-                      fontSize: "1.4rem",
-                    }}
+                  <Text
+                    margin="8px 0 0 7px"
+                    bold='600'
+                    size="1.4rem"
                   >
                     어디어디 산의 어디 코스
-                  </p>
+                  </Text>
 
                   <Grid
                     height="20px"
@@ -171,8 +147,8 @@ const Main = () => {
                     width="194px"
                     margin="8px 7px 0 7px"
                   >
-                    <span style={{ fontWeight: "300" }}>매우 좋음 5.0</span>
-                    <span style={{ fontWeight: "400" }}>100.800km</span>
+                    <Text bold="300" size="1.2rem">매우 좋음 5.0</Text>
+                    <Text bold="400" size="1.2px">100.800km</Text>
                   </Grid>
                 </div>
               ))}
@@ -181,17 +157,15 @@ const Main = () => {
 
           <Grid height="220px">
             <Grid margin="0 0 24px 0" height="25px" isFlex>
-              <p
-                style={{
-                  width: "350px",
-                  height: "24px",
-                  fontWeight: "600",
-                  fontSize: "2rem",
-                  lineHeight: "24px",
-                }}
+              <Text
+                width="350px"
+                height="24px"
+                bold="600"
+                size="2rem"
+                lineHeight="24px"
               >
                 👀 실시간 정복한 산길 인증샷
-              </p>
+              </Text>
               <Grid
                 margin="0 18px 0 0"
                 fontSize="12px"
