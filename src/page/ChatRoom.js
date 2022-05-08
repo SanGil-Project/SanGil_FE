2a8e02e16c53c5b6e6fc3dde76551b4fc8fa8bdc
parkksss <yesleee9211@gmail.com>
2022-05-08 18:20:01 +0900

    채팅방 페이지 뷰 완성(와이어프레임 베이스)

--- src/page/ChatRoom.js
+++ src/page/ChatRoom.js
@@ -15,13 +15,60 @@ const ChatRoom = (props) => {
     userTitle: "홍길동",
   };
 
+  const participants = [
+    {
+      username: "정상준",
+      userTitle: "홍길동",
+      userImageUrl: "https://user-images.githubusercontent.com/91959791/163972509-ca46de43-33cf-4648-a61d-47f32dfe20b3.png",
+    },
+    {
+      username: "박예슬",
+      userTitle: "등린이",
+      userImageUrl: "https://user-images.githubusercontent.com/91959791/163972509-ca46de43-33cf-4648-a61d-47f32dfe20b3.png",
+    },
+    {
+      username: "정의현",
+      userTitle: "산길인맥왕",
+      userImageUrl: "https://user-images.githubusercontent.com/91959791/163972509-ca46de43-33cf-4648-a61d-47f32dfe20b3.png",
+    },
+    {
+      username: "천누리",
+      userTitle: "아직 여기라고?",
+      userImageUrl: "https://user-images.githubusercontent.com/91959791/163972509-ca46de43-33cf-4648-a61d-47f32dfe20b3.png",
+    },
+  ];
+
   return (
     <React.Fragment>
       <ChatContainer>
         <Header />
         <ChatWrap>
           <Grid padding="96px 14px 100px">
-            <Grid flexRow margin="0 0 20px">
+            <Grid flexRow margin="10px 0 0">
+              <Image
+                type="circle"
+                width="32px"
+                margin="0 14px 0 0"
+                src={participants[2].userImageUrl}/>
+              <Grid>
+                <Text margin="0" size="12px" bold="500">[{participants[2].userTitle}] {participants[2].username}</Text>
+              </Grid>
+            </Grid>
+            <Grid padding="5px 0 5px 46px" flexRow justify>
+              <Grid padding="16px" bg="#EBEBEB" radius="30px" width="auto">
+                <Text margin="0">등산에 필요한 물품은 각자 알아서 </Text>
+              </Grid>
+              {/* <Text margin="35px 0 0" size="12px" bold="500" color="#A4A4A4">오전 8:20</Text> */}
+            </Grid>
+            <Grid padding="5px 0 5px 46px" flexRow justify>
+              <Grid padding="16px" bg="#EBEBEB" radius="30px" width="auto">
+                <Text margin="0">가져오는 걸로 해요 !!! </Text>
+              </Grid>
+              <Text margin="35px 0 0" size="12px" bold="500" color="#A4A4A4">오전 8:20</Text>
+            </Grid>
+
+
+            <Grid flexRow margin="10px 0 0">
               <Image
                 type="circle"
                 width="32px"
@@ -31,9 +78,17 @@ const ChatRoom = (props) => {
                 <Text margin="0" size="12px" bold="500">[{userInfo.userTitle}] {userInfo.username}</Text>
               </Grid>
             </Grid>
+            <Grid padding="5px 0 5px 46px" flexRow justify>
+              <Grid padding="16px" bg="#68DC68" radius="30px" width="auto">
+                <Text margin="0">등산에 필요한 물품은 각자 알아서 </Text>
+              </Grid>
+              <Text margin="35px 0 0" size="12px" bold="500" color="#A4A4A4">오전 8:20</Text>
+            </Grid>
           </Grid>
         </ChatWrap>
         
+        
+
         <MenubarContainer>
           <Grid height="88px" minWidth="414px" maxWidth="800px" margin="auto">
             <Menubar menuColor={menuColor}/>
