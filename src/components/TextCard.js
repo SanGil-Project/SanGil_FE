45a83308afee40c999c81c1430880e59e6241249
parkksss <yesleee9211@gmail.com>
2022-05-13 17:07:57 +0900

    - 마이페이지 feedlist 가져오기 api 연결완료 - 마이페이지 레이아웃 조정

--- src/components/TextCard.js
+++ src/components/TextCard.js
@@ -12,6 +12,7 @@ const TextCard = (props) => {
   const moveDetail = (partyId) => {
     navigate(`/partydetail/${partyId}`, {state: {partyId: partyId}});
   }
+  console.log(data);
 
   return (
     <React.Fragment>
@@ -19,10 +20,10 @@ const TextCard = (props) => {
         <Grid _onClick={()=>{moveDetail(data.partyId)}} hover>
           <Grid isFlex height="auto" margin="0 0 4px">
             <Text margin="0" bold="600" size="14px">{data.title}</Text>
-            <Text margin="0" bold="200" size="12px">{data.curPeople}/{data.maxPeople}명</Text>
+            <Text margin="0" bold="300" size="12px">{data.curPeople}/{data.maxPeople}명</Text>
           </Grid>
           <Grid isFlex height="auto">
-            <Text margin="0" bold="400" size="12px">{data.partyDate}</Text>
+            <Text margin="0" bold="600" size="12px">{data.partyDate}</Text>
             <Text margin="0" bold="300" size="12px" color="#c4c4c4">자세한 내용 보기</Text>
           </Grid>
         </Grid>
