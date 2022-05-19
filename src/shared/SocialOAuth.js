1ef66a47ebd956de99a62ebc687a81f197fb1523
parkksss <yesleee9211@gmail.com>
2022-05-19 23:43:16 +0900

    - 전체 폰트 Roboto 설정

--- src/shared/SocialOAuth.js
+++ src/shared/SocialOAuth.js
@@ -4,10 +4,13 @@ const REDIRECT_URI = "http://localhost:3000/user/kakao/callback";
 // const REDIRECT_URI = "https://kopite.shop/user/kakao/callback";
 
 const GOOGLE_CLIENT_ID =
-  "77683946484-86n78jead6i4agakkjdf3482c3609des.apps.googleusercontent.com";
+  "418085046130-194eqk6jjjmn32gnfiv6g331ooh985de.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_ID =
+//   "77683946484-86n78jead6i4agakkjdf3482c3609des.apps.googleusercontent.com";
 // const GOOGLE_REDIRECT_URI = "http://localhost:3000/user/google/callback";
 // const GOOGLE_REDIRECT_URI = "https://xn--wk0b636a.com/user/google/callback";
-const GOOGLE_REDIRECT_URI = "https://kopite.shop/user/google/callback";
+// const GOOGLE_REDIRECT_URI = "https://kopite.shop/user/google/callback";
+const GOOGLE_REDIRECT_URI = "http://localhost:3000/user/google/callback";
 
 const NAVER_CLIENT_ID = "oq32J_8jgLtjcSRvYUO4";
 const NAVER_CLIENT_SECRET = "dc6LwAfBEL";
